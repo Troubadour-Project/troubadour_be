3d334c00ab6f75b6cf5fb2df82fc6d35f631d85e
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-25 09:33:50 -0600

    Feat: Add query class `FetchUser` and add entry point to type class `QueryType`

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,5 +4,6 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :fetch_users, resolver: Queries::FetchUsers, null: false, description: 'Returns a list of users'
+    field :fetch_user, resolver: Queries::FetchUser
   end
 end
