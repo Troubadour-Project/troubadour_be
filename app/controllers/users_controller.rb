2fe7490d233a8009368cd7395f52d750bf36c47b
Tim Roeder <timothyjroeder@gmail.com>
2022-03-26 16:27:31 -0600

    Request: add test route for active storage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    user.profile.attach(user_params[:profile])
+    user.video.attach(user_params[:video])
+    
+    redirect_to(url_for(user.profile))
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :profile, :video)
+  end
+end
