c59aadc416aaf87c34313966773b2d2969291435
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-27 10:57:02 -0600

    Update: Permit all user attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
     email = { email: user_params[:email] }
-    user = User.create!(email)
+    user = User.create!(user_params)
     user.profile.attach(user_params[:profile])
     user.video.attach(user_params[:video])
 
@@ -11,6 +11,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:email, :profile, :video)
+    params.permit(:email, :profile, :video, :genre, :name, :song_title)
   end
 end
