9a344608c37afe6706404ca37df7651bc9d4aa91
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-29 21:02:27 -0600

    Data: add winner column to submissions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_011801) do
+ActiveRecord::Schema.define(version: 2022_03_29_214128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2022_03_29_011801) do
     t.string "song_title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "winner"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
