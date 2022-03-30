20cc1527faae3c1ad772c8f853e4dcc14a1343d6
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-29 21:27:32 -0600

    Data: Update winner to be default of nil for submission

--- db/migrate/20220329214128_add_winner_to_submissions.rb
+++ db/migrate/20220329214128_add_winner_to_submissions.rb
@@ -1,5 +1,5 @@
 class AddWinnerToSubmissions < ActiveRecord::Migration[6.1]
   def change
-    add_column :submissions, :winner, :boolean
+    add_column :submissions, :winner, :boolean, :default => nil
   end
 end
