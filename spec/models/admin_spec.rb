ac7a976078971c1b11e2b43039a36a176ed9b007
Tim Roeder <timothyjroeder@gmail.com>
2022-03-30 22:23:20 -0600

    Test: add validation and factory tests

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+  end
+
+  describe 'factory model' do
+    it 'exists with attributes' do
+      admin = build(:admin, username: 'best_admin88', email: 'example@email.com')
+
+      expect(admin).to be_a(Admin)
+      expect(admin).to have_attributes(username: 'best_admin88')
+      expect(admin).to have_attributes(email: 'example@email.com')
+    end
+  end
 end
