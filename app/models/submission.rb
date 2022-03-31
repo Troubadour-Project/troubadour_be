8aed7e559121eceddbbb36757dad3b1f283c1110
Tim Roeder <timothyjroeder@gmail.com>
2022-03-30 22:37:24 -0600

    Model: update for merge

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,9 +1,12 @@
 class Submission < ApplicationRecord
-  validates_presence_of :email
-  validates_presence_of :name
-  validates_presence_of :genre
-  validates_presence_of :song_title
-  validates_presence_of :winner, :allow_nil => true
   has_one_attached :profile
   has_one_attached :video
+  has_many :submission_admins
+  has_many :admins, through: :submission_admins
+  
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :genre
+  validates_presence_of :song_title
+  validates_presence_of :winner
 end
