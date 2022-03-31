eecaa00e91fd3ec9b57137faa6a185f68df1b174
Tim Roeder <timothyjroeder@gmail.com>
2022-03-30 22:18:51 -0600

    Factory: create model factories

--- spec/factories/submission_admins.rb
+++ spec/factories/submission_admins.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :submission_admin do
-    selected { false }
-    admin { nil }
-    submission { nil }
+    favorite { false }
+    admin
+    submission
   end
 end
--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
   factory :submission do
-    name { "MyString" }
-    email { "MyString" }
-    genre { "MyString" }
-    song_title { "MyString" }
+    name { Faker::Music.band }
+    email { Faker::Internet.email }
+    genre { Faker::Music.genre }
+    song_title { Faker::Music::RockBand.song }
+    winner { false }
+    profile { Rack::Test::UploadedFile.new('spec/fixtures/blank_profile_photo.jpeg', 'image/jpeg') }
+    video { Rack::Test::UploadedFile.new('spec/fixtures/pexels_stock_video.mp4', 'image/mp4') }
   end
 end
