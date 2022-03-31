094f86e31ad1d565ea9a248dd30a4a4529aee20a
Tim Roeder <timothyjroeder@gmail.com>
2022-03-30 22:21:42 -0600

    Query: add get_winner query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
+    # Submission Queries
     field :get_submission, resolver: Queries::GetSubmission, description: 'Returns a submissions data'
     field :get_submissions, resolver: Queries::GetSubmissions, null: true, description: 'Returns a list of submissions'
-    # field :get_winning_submissions, resolver: Queries::GetWinningSubmissions, null: true, description: 'Returns a list of winning submissions'
+    field :get_winner, resolver: Queries::GetWinner, null: true, description: 'Returns a winning submission if there is one'
 
+    # Admin Queries
     field :get_admin, resolver: Queries::GetAdmin, null: true, description: 'Returns a specific admins data'
-
-    #  def get_submissions
-    #    Submission.all
-    #  end
   end
 end
