4c8e0dce840cff7ae979b0bd5b7d51fdb7ba8904
Tim Roeder <timothyjroeder@gmail.com>
2022-03-31 16:29:08 -0600

    Test: add invalid request edge case test

--- spec/requests/api/v1/submissions_request_spec.rb
+++ spec/requests/api/v1/submissions_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Submissions Request' do
   describe 'request to create a submission' do
     describe 'POST /submissions' do
-      describe 'sucessful request' do
+      context 'sucessful request' do
         it 'renders the JSON creation of the submission object and created status' do
           sub_params = ({
                       name: 'Sierra',
@@ -48,9 +48,28 @@ RSpec.describe 'Submissions Request' do
 
           expect(new_sub.submission_admins.count).to eq(5)
           expect(new_sub.submission_admins.first).to be_a(SubmissionAdmin)
-          
+
           expect(admin_list[0].submission_admins.count).to eq(1)
           expect(admin_list[0].submission_admins.first).to be_a(SubmissionAdmin)
+        end
+      end
+
+      context 'unsuccessful request' do
+        it 'renders the JSON error status when given incorrect params' do
+          sub_params = ({
+            name: 'Sierra',
+            email: 'test@email.com',
+            song_title: 'Software Jam',
+            profile: fixture_file_upload('users_profile_submission.JPG', 'image/jpg'),
+            video: fixture_file_upload('users_video_submission.mp4', 'video/mp4'),
+            winner: false
+            })
+          headers= {"CONTENT_TYPE" => "multipart/form-data"}
+          post "/api/v1/submissions", headers: headers, params: {submission: sub_params}
+          json = json_parse(response.body)
+
+          expect(json[:errors]).to eq("Validation failed: Genre can't be blank")
+          expect(response.status).to eq(400)
         end
       end
     end
