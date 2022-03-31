6d122f3edbbed3e1bfca90f3f4e75f1abf987517
Tim Roeder <timothyjroeder@gmail.com>
2022-03-31 15:09:28 -0600

    Request: create submission_admin

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SubmissionsController < ApplicationController
   def create
     submission = Submission.create!(submission_params)
+    attach_submission_admin(submission)
     render json: SubmissionsSerializer.new(submission), status: 201
   end
 
@@ -8,5 +9,11 @@ class Api::V1::SubmissionsController < ApplicationController
 
   def submission_params
     params.require(:submission).permit(:email, :profile, :video, :genre, :name, :song_title, :winner)
+  end
+
+  def attach_submission_admin(submission)
+    Admin.all.each do |admin|
+      SubmissionAdmin.create!(admin_id: admin.id, submission_id: submission.id)
+    end
   end
 end
