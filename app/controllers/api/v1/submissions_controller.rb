b951b45e9556c9002d1e6757c6d2737877feb40e
Tim Roeder <timothyjroeder@gmail.com>
2022-04-02 14:02:30 -0600

    Request: moved submission_admin creation to model

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SubmissionsController < ApplicationController
   def create
     submission = Submission.create!(submission_params)
-    attach_submission_admin(submission)
 
     json_response(SubmissionsSerializer.new(submission), :created)
   end
@@ -10,11 +9,5 @@ class Api::V1::SubmissionsController < ApplicationController
 
   def submission_params
     params.require(:submission).permit(:email, :profile, :video, :genre, :name, :song_title, :winner)
-  end
-
-  def attach_submission_admin(submission)
-    Admin.all.each do |admin|
-      SubmissionAdmin.create!(admin_id: admin.id, submission_id: submission.id)
-    end
   end
 end
