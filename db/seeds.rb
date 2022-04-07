be7a87191390a23037645493728beadebc4085f5
Tim Roeder <timothyjroeder@gmail.com>
2022-04-07 10:15:24 -0600

    DB: seed just the admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,24 +12,24 @@ admin = Admin.create(username: 'admin1', email: Faker::Internet.email)
 # photo = fixture_file_upload('blank_profile_photo.jpeg', 'image/jpeg')
 # vid = fixture_file_upload('pexels_stock_video.mp4', 'video/mp4')
 
-5.times do |u|
-  submission = Submission.new(
-                          name: "sub#{u+1}", 
-                          email: Faker::Internet.email, 
-                          song_title: Faker::Lorem.word, 
-                          genre: Faker::Lorem.word, 
-                          winner: nil,
-                         )
-  submission.profile.attach(
-    io: File.open(Rails.root.join('spec/fixtures/blank_profile_photo.jpeg')), 
-    filename: 'blank_profile_photo.jpeg', 
-    content_type: 'image/jpeg'
-  )
-  submission.video.attach(
-    io: File.open(Rails.root.join('spec/fixtures/pexels_stock_video.mp4')), 
-    filename: 'pexels_stock_video.mp4', 
-    content_type: 'video/mp4'
-  )
-  submission.save!
-  SubmissionAdmin.create(admin_id: admin.id, submission_id: submission.id)
-end
+# 5.times do |u|
+#   submission = Submission.new(
+#                           name: "sub#{u+1}", 
+#                           email: Faker::Internet.email, 
+#                           song_title: Faker::Lorem.word, 
+#                           genre: Faker::Lorem.word, 
+#                           winner: nil,
+#                          )
+#   submission.profile.attach(
+#     io: File.open(Rails.root.join('spec/fixtures/blank_profile_photo.jpeg')), 
+#     filename: 'blank_profile_photo.jpeg', 
+#     content_type: 'image/jpeg'
+#   )
+#   submission.video.attach(
+#     io: File.open(Rails.root.join('spec/fixtures/pexels_stock_video.mp4')), 
+#     filename: 'pexels_stock_video.mp4', 
+#     content_type: 'video/mp4'
+#   )
+#   submission.save!
+#   SubmissionAdmin.create(admin_id: admin.id, submission_id: submission.id)
+# end
